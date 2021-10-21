e84d846524b07f1f03ffb2abb856823b2aeede7e
pingjuh <hsieping12@gmail.com>
2021-10-22 00:50:16 +0800

    Add blue and red bluetooh svg

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import NavItem from './NavItem';
 import DropdownMenu from './DropdownMenu';
 import { ReactComponent as CaretIcon } from '../../icons/caret.svg';
-import { ReactComponent as Bluetooth } from '../../icons/bluetooth.svg';
+import { ReactComponent as BluetoothBlue } from '../../icons/bluetoothblue.svg';
+import { ReactComponent as BluetoothRed } from '../../icons/bluetoothred.svg';
+import SensorContext from '../../context/sensor/sensorContext';
 
-const Navbar = () => (
-  <nav className="navbar">
-    <ul className="navbar-nav">
-      <NavItem icon="📊" link="/"/>
-      <NavItem icon={<Bluetooth />} link="connect" />
-      <NavItem icon={<CaretIcon />} >
-        <DropdownMenu/>
-      </NavItem>
-    </ul>
-  </nav>
-);
+const Navbar = () => {
+  const { connected } = useContext(SensorContext);
+  
+  if (!connected) return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <NavItem icon="📊" link="/"/>
+        <NavItem icon={<BluetoothRed />} link="connect" />
+        <NavItem icon={<CaretIcon />} >
+          <DropdownMenu/>
+        </NavItem>
+      </ul>
+    </nav>
+  )
+
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav">
+        <NavItem icon="📊" link="/"/>
+        <NavItem icon={<BluetoothBlue />} link="connect" />
+        <NavItem icon={<CaretIcon />} >
+          <DropdownMenu/>
+        </NavItem>
+      </ul>
+    </nav>
+  )
+};
 
 export default Navbar;
