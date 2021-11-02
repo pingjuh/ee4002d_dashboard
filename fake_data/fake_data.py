954940dbb8267a09bb89c17822032535668b027b
pingjuh <hsieping12@gmail.com>
2021-11-02 15:50:57 +0800

    Add classifications and sensors collections

--- fake_data/fake_data.py
+++ fake_data/fake_data.py
@@ -15,13 +15,15 @@ class sensor(Thread):
     def run(self):
         cluster = MongoClient(mongoURI())
         db = cluster["EE4002D_dashboard"]
-        collection = db["cachedsensors"]
+        collection_cached = db["cachedsensors"]
+        collection = db["sensors"]
         # Create an expiring (TTL) collection
         # The background task that removes expired documents runs every 60 seconds. As a result, documents may remain in a collection   
         # during the period between the expiration of the document and the running of the background task.
-        collection.create_index("inserted", expireAfterSeconds = 60)
+        collection_cached.create_index("inserted", expireAfterSeconds = 60)
         while True:
             fake_data = {"sensorsReading":[random.randint(0,255) for i in range(12)], "inserted":datetime.datetime.utcnow()}
+            collection_cached.insert_one(fake_data)
             collection.insert_one(fake_data)
             sleep(0.10)
 
@@ -37,11 +39,14 @@ class classification(Thread):
     def run(self):
         cluster = MongoClient(mongoURI())
         db = cluster["EE4002D_dashboard"]
-        collection = db["cachedclassifications"]
-        collection.create_index("inserted", expireAfterSeconds = 60)
+        collection_cached = db["cachedclassifications"]
+        collection = db["classifications"]
+        collection_cached.create_index("inserted", expireAfterSeconds = 60)
         while True:
             fake_data = {"classification":random.randint(0,15), "inserted":datetime.datetime.utcnow()}
+            collection_cached.insert_one(fake_data)
             collection.insert_one(fake_data)
+            
             sleep(5)
       
 sensor()
