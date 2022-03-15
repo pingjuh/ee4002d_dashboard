b34361cd6dbc1998b94c33705a82d28baee0fd86
pingjuh <hsieping12@gmail.com>
2022-03-15 13:48:25 +0800

    Remove board gif

--- client/src/components/board/Board.js
+++ client/src/components/board/Board.js
@@ -1,16 +1,7 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import boardjpeg from './board.jpeg'
-import boardgif from './board.gif'
-import sensorContext from '../../context/sensor/sensorContext'
 
-const Board = () => {
-  const { connected } = useContext(sensorContext)
-  if (connected) return (
-    <img src={boardgif} alt="board" style={{ width: '201px', height: '390px', margin: 'auto', display: 'block' }} />
-  );
-  else return (
-    <img src={boardjpeg} alt="board" style={{ width: '201px', height: '390px', margin: 'auto', display: 'block' }} />
-  );
-}
-    
+const Board = () =>
+  <img src={boardjpeg} alt="board" style={{ width: '201px', height: '390px', margin: 'auto', display: 'block' }} />
+
 export default Board;
